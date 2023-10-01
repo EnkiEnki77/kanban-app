b47a05593bc99b54c3fc13dc8e080e313997837c
Enki Layman <layman212@gmail.com>
2023-10-01 05:55:09 -0400

    Added Markup For Header Component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./Button.css"
 
-const Button = ({btnModifier}) => {
+const Button = ({btnModifier, children}) => {
   return (
-    <button className={`btn ${btnModifier}`}>Button</button>
+    <button className={`btn ${btnModifier}`}>{children}</button>
   )
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Logo from '../logo/Logo'
+import Button from '../button/Button'
+import BoardSettings from '../../assets/icon-vertical-ellipsis.svg'
 
-const Header = () => {
+const Header = ({boardTitle}) => {
   return (
     <header className='header'>
-        
+        <Logo/>
+        <h1 className='board-title'>{boardTitle}</h1>
+        <Button buttonModifier=''>
+            + Add New Task
+        </Button>
+        <img className='board-settings' src={BoardSettings} alt='board settings'/>
     </header>
   )
 }
